743ace2dae55d9afef221a45512e072ee4016299
Tommy Calvy <42327902+tommycalvy@users.noreply.github.com>
2023-11-09 22:11:44 -0600

    jspdf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import { jsPDF } from "jspdf";
+
+    // Default export is a4 paper, portrait, using millimeters for units
+    const doc = new jsPDF();
+
+    doc.text("Hello world!", 10, 10);
+    doc.save("a4.pdf");
+</script>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
